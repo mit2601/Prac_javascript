26c160ac2f2b7c2f1c18e1ee95ea255165cbab51
Mit Patel <pmeet5794@gmail.com>
2026-08-05 17:26:36 +0530

    updates

--- 5aug.js
+++ 5aug.js
@@ -25,3 +25,79 @@
 
 // console.log(employees.employeeList[4].name)
 
+
+// function x(){
+//     let a = 10;
+
+//     function y(){
+//         console.log(a);
+//     }
+//     y();
+// }
+
+// x();
+
+
+// function x(){
+//     let a = 10;
+
+// //    return function y(){
+// //         console.log(a);
+// //     }
+// function y(){
+//         console.log(a);
+//     }
+//     a=100;
+
+//   return y;
+// }
+
+// const z = x();
+// //console.log(z);
+// // console.log(z());
+// z();
+
+// function sum(a) {
+
+//     return function (b) {
+
+//         return function (c) {
+
+//             return a + b + c;
+
+//         };
+
+//     };
+
+// }
+
+// console.log(sum(10)(20)(30));
+
+
+// function calculatePrice(tax) {
+
+//     return function (price) {
+
+//         return price + price * tax;
+
+//     };
+
+// }
+
+// const gst18 = calculatePrice(0.18);
+
+// console.log(gst18(1000));
+// console.log(gst18(500));
+// console.log(gst18(700));
+
+
+
+// function greet(name) {
+
+//     setTimeout(() => {
+//         console.log(`Hello ${name}`);
+//     }, 2000);
+
+// }
+
+// greet("Mit");
